cd213f4e6cf92be3b5e7ea7270e1bfc871fc4d6b
srinivasrk <srinivasrk92@gmail.com>
2019-05-23 15:19:42 +0000

    right way to close rabbitmq connection

--- consumer.py
+++ consumer.py
@@ -21,13 +21,14 @@ logger.info("Created logger")
 received_signal = False
 processing_callback = False
 
-
 def signal_handler(signal, frame):
     global received_signal
-    print("signal received")
+    global channel
+    global connection
+    logger.info("signal received")
     received_signal = True
-    if not processing_callback:
-         sys.exit()
+    if not processing_callback and channel is not None and connection is not None:
+         channel.cancel()
 
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -37,9 +38,11 @@ def callback(ch, method, properties, body):
     logger.info("Message received")
     logger.info(body)
     global processing_callback
+    global channel
+    global connection
+
     processing_callback = True
     try:
-        logger.info(body)
         sum(range(0, 10000)) # sleep gets interrupted by signals, this doesn't.
         ch.basic_ack(delivery_tag=method.delivery_tag)
         logger.info("Message consumption complete")
@@ -48,10 +51,19 @@ def callback(ch, method, properties, body):
     finally:
         processing_callback = False
         if received_signal:
-            sys.exit()
+            channel.cancel()
 
+
+def closeConnection(method_frame):
+    global connection
+    global channel
+    channel.close()
+    connection.close()
 
 if __name__ == "__main__":
+    global channel
+    global connection
+
     try:
         _credentials = pika.PlainCredentials('guest',
                                              'guest')
@@ -61,12 +73,13 @@ if __name__ == "__main__":
             5672,
             '/',
             _credentials, heartbeat=0, socket_timeout=360))
-
+        connection = _connection
         channel = _connection.channel()
+        channel.add_on_cancel_callback(closeConnection)
         logger.info(' [*] Waiting for messages. To exit press CTRL+C')
         channel.queue_declare(queue='test', durable=True)
         channel.basic_consume(queue='test', on_message_callback=callback)
         channel.start_consuming()
     except Exception as e:
-        channel.close()
-        sys.exit()
+        logger.info(e)
+        channel.cancel()
